e9c2ae51ecb8098ec8087aaf65d5e8f820f7df94
Kelvin <abellavinkel@gmail.com>
2020-02-01 16:24:42 +0400

    added service worker

--- client/src/index.js
+++ client/src/index.js
@@ -7,6 +7,7 @@ import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
 import { store, persistor } from "./redux/store";
+import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
   <Provider store={store}>
@@ -18,3 +19,4 @@ ReactDOM.render(
   </Provider>,
   document.getElementById("root")
 );
+serviceWorker.register();
